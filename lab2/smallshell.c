921a751d792d26b794e876e2b21b2dded782ce4c
Cecilia Roes <cecilia.roes@gmail.com>
2013-04-21 15:44:04 +0200

    shell now supports cd

--- lab2/smallshell.c
+++ lab2/smallshell.c
@@ -6,9 +6,13 @@
 
 #include <sys/types.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define MAX_COMMAND_SIZE 80
 #define MAX_NB_ARGUMENTS 6
+#define MAX_PATH_LENGTH 100
 #define FOREGROUND 0
 #define BACKGROUND 1
 
@@ -25,16 +29,42 @@ int main(int argc, char *argv[]){
   char cmd_string[MAX_COMMAND_SIZE];
   char *cmd_argv[MAX_NB_ARGUMENTS];
   int mode;
+  char *home = getenv("HOME");
+  char current_wd[MAX_PATH_LENGTH];
+  getcwd(current_wd, MAX_PATH_LENGTH);
 
   while(1){
+    int return_value;
+
 
     /*
      * Read and parse commandline input.
      */
 
     mode = FOREGROUND;
+    fprintf(stdout, "%s$ ", current_wd);
     fgets(cmd_string, MAX_COMMAND_SIZE, stdin);
     parse_command(cmd_string, cmd_argv, &mode);
+
+
+    if(cmd_argv[0] == '\0') continue;   /* No command read, continue */
+
+
+    /*
+     * Change working directory.
+     */
+
+    else if(strcmp(cmd_argv[0],"cd") == 0){
+      if (cmd_argv[1] != '\0'){         /* If a path is supplied, change to it */
+        return_value = chdir(cmd_argv[1]);
+      }
+      if (cmd_argv[1] == '\0' ||        /* If no argument or change failed */
+          return_value == -1){          /* change to home */
+        return_value = chdir(home);
+        if (return_value == -1) fprintf(stderr, "Could not cd to HOME");
+      }
+      getcwd(current_wd, MAX_PATH_LENGTH);
+    }
 
 
   }
@@ -47,8 +77,8 @@ void parse_command(char *cmd_string, char *cmd_argv[], int *mode){
   while(*p != '\0'){
     *cmd_argv = p;                      /* Adds word pointer from input to
                                            argument array */
-    while(*p != ' ' && *p != '\0'){     /* Loops past all chars in current
-                                           word */
+    while(*p != ' ' && *p != '\0' &&
+          *p != '\n'){                  /* Loops past all chars in current word */
       if(*p == '&') *mode = BACKGROUND;
       p++;
     }
