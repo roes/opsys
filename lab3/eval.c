3baa97f8e0a89abb93b761281f3dc4bec702f5f5
Cecilia Roes <cecilia.roes@gmail.com>
2013-05-14 11:21:06 +0200

    added more eval test

--- lab3/eval.c
+++ lab3/eval.c
@@ -3,10 +3,9 @@
 
 #include "tst.h"
 
-#define TIMES 100
+#define TIMES 100                       /* Number of times to run the test cases */
 
-#define MAX(a,b) ((a > b)?(a):(b))
-#define MIN(a,b) ((a > b)?(b):(a))
+#define MAX(a,b) ((a>b)?(a):(b))
 
 union header {
   struct {
@@ -16,54 +15,83 @@ union header {
 };
 typedef union header Header;
 
-void test(int data_size, char* test_name, int size){
+
+/*
+ *
+ */
+void test(char* test_name,              /* Name of the test */
+          char mode,                    /* increase (+), decrease (-), random (?) or constant (=) */
+          int start_size,               /* Initial size to malloc */
+          int num_vars){                /* Number of variables to malloc */
   int i, j;
-  double avg_memusage = 0;
-  char *a[size];
-  void *lowmem, *highmem, *maxmem = 0, *minmem = 0;
+  unsigned long max_memusage = 0;       /* Over TIMES runs, maximum memory requested */
+  char *a[num_vars];                    /* Pointers to the allocated memory */
+  void *lowmem, *highmem;               /* Pointers to lower and upper usage on the heap */
 
-  fprintf(stderr, "Running predicted %s case evaluation.\n", test_name);
+  fprintf(stderr, "Running %s case evaluation.\n", test_name);
 
-#ifdef MMAP
+#ifdef MMAP                             /* Set lower heap pointer */
   lowmem = endHeap();
 #else
   lowmem = (void *) sbrk(0);
 #endif
 
   for(i = 0; i < TIMES; i++){
+    int data_size = start_size;         /* Parameter to malloc request */
+    unsigned long memusage = 0;         /* Total memory requested through malloc() */
     /* Start timing */
-    for(j = 0; j < size; j++){
+    for(j = 0; j < num_vars; j++){
+      switch(mode){
+        case '=':                       /* Constant malloc allocation */
+          break;
+        case '+':                       /* Amount allocated by malloc is increasing */
+          data_size += sizeof(Header);
+          break;
+        case '-':                       /* Amount allocated by malloc is decreasing */
+          data_size -= sizeof(Header);
+          break;
+        case '?':                       /* Amount allocated by malloc is random */
+          data_size = 1;                /* TODO: randomize */
+          break;
+      }
       a[j] = malloc(data_size);
+      memusage += data_size;
     }
     /* Stop timing */
-#ifdef MMAP
-    highmem = endHeap();
-#else
-    highmem = (void *) sbrk(0);
-#endif
-    maxmem = MAX(maxmem, highmem);
-    minmem = (minmem == 0)?maxmem:MIN(minmem,highmem);
-    avg_memusage += (highmem-lowmem)*1.0/TIMES;
-
-    for(j = 0; j < size; j++){
+    for(j = 0; j < num_vars; j++){
       free(a[j]);
     }
+    max_memusage = MAX(max_memusage, memusage);/* Only varies for random mode */
   }
 
-  fprintf(stderr, "Memory usage: minimum usage - 0x%x\t maximum usage - 0x%x \taverage usage - 0x%x\n", (unsigned) (minmem-lowmem), (unsigned) (maxmem-lowmem), (unsigned) avg_memusage);
-  fprintf(stderr, "Memory needed: 0x%x\n", (unsigned) data_size*size);
+#ifdef MMAP                             /* Set upper heap pointer*/
+  highmem = endHeap();
+#else
+  highmem = (void *) sbrk(0);
+#endif
+
+  fprintf(stderr, "Memory usage: maximum usage - 0x%x\n", (unsigned) (highmem-lowmem));
+  fprintf(stderr, "Memory needed: 0x%x\n", (unsigned) max_memusage);
 }
 
 void bestCase(){
-  test(sizeof(Header)*1023, "best", 10000);
+  test("predicted best", '=', sizeof(Header)*1023, 10000);
 }
 
 void worstCase(){
-  test(sizeof(Header)*512, "worst", 10000);
+  test("predicted worst", '=', sizeof(Header)*512, 10000);
 }
 
 void randomSizes(){
+  test("randomized", '?', sizeof(Header)*1, 2000);
+}
 
+void incrementSizes(){
+  test("increment", '+', 0, 2000);
+}
+
+void decrementSizes(){
+  test("decrement", '-', sizeof(Header)*2001, 2000);
 }
 
 int main(int argc, char *argv[]){
@@ -88,6 +116,14 @@ int main(int argc, char *argv[]){
     case 'r':
       MESSAGE("Starting evaluation of malloc performance given random data\n");
       randomSizes();
+      break;
+    case '+':
+      MESSAGE("Starting evaluation of malloc performance given increasing malloc sizes\n");
+      incrementSizes();
+      break;
+    case '-':
+      MESSAGE("Starting evaluation of malloc performance given decreasing malloc sizes\n");
+      decrementSizes();
       break;
   }
   return 0;
